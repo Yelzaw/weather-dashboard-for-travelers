2186bb5eec83db2ff075dea888c570f8ae350b70
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2022-11-24 23:26:13 -0400

    Built Html and Fetch some data

---
+++ assets/js/script.js
@@ -0,0 +1,108 @@
+var searchFormEl = document.querySelector('#search-form');
+var searchHistoryButtonsEl = document.querySelector('#search-history-buttons');
+var cityInputEl = document.querySelector('#cityname');
+var cityDataSearch = document.querySelector('#city-data-search');
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var cityname = cityInputEl.value.trim();
+
+  if (cityname) {
+    getCityData(cityname);
+
+    // repoContainerEl.textContent = '';
+    cityInputEl.value = '';
+  } else {
+    alert('Please enter a city name');
+  }
+};
+
+// var buttonClickHandler = function (event) {
+//   var language = event.target.getAttribute('data-language');
+
+//   if (language) {
+//     getFeaturedRepos(language);
+
+//     repoContainerEl.textContent = '';
+//   }
+// };
+
+var getCityData = function (city) {
+  var apiKey = "916dbda628645c79f5666248b6b1d5d5"
+  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="+apiKey+"&units=imperial";//Fahrenheit,Imperial: miles/hour.
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          console.log(data.list[1].dt_txt);
+          console.log(data.list[1].main.humidity);
+          console.log(data.list[1].main.temp);
+          console.log(data.list[1].wind.speed);
+
+          // displayRepos(data, city);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to Openweathermap');
+    });
+};
+
+// var getFeaturedRepos = function (language) {
+//   var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         displayRepos(data.items, language);
+//       });
+//     } else {
+//       alert('Error: ' + response.statusText);
+//     }
+//   });
+// };
+
+// var displayRepos = function (repos, searchTerm) {
+//   if (repos.length === 0) {
+//     repoContainerEl.textContent = 'No repositories found.';
+//     return;
+//   }
+
+//   repoSearchTerm.textContent = searchTerm;
+
+//   for (var i = 0; i < repos.length; i++) {
+//     var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+//     var repoEl = document.createElement('a');
+//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
+//     repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+
+//     var titleEl = document.createElement('span');
+//     titleEl.textContent = repoName;
+
+//     repoEl.appendChild(titleEl);
+
+//     var statusEl = document.createElement('span');
+//     statusEl.classList = 'flex-row align-center';
+
+//     if (repos[i].open_issues_count > 0) {
+//       statusEl.innerHTML =
+//         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+//     } else {
+//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//     }
+
+//     repoEl.appendChild(statusEl);
+
+//     repoContainerEl.appendChild(repoEl);
+//   }
+// };
+
+searchFormEl.addEventListener('submit', formSubmitHandler);
+// languageButtonsEl.addEventListener('click', buttonClickHandler);
